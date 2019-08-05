87186f9cce2bc2d3e0b5265c21c1b37e2e4bd4d2
Jason.Lam <luan.gia.lam@gmail.com>
2019-08-04 19:47:39 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearhBar from './SearchBar';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <SearhBar />
+            </div>
+        );
+    }
+}
+
+export default App;
